d13cdb58bb5acd06e356c91b95d639ff0167f534
Ashkan Heidari <ashkanheidari@Ashkans-MacBook-Air.local>
2024-08-23 17:25:18 -0700

    Implement another solution for the problem

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -1,14 +1,14 @@
-import collections
-class Solution:
-    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        ans = collections.defaultdict(list)
+# import collections
+# class Solution:
+#     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+#         ans = collections.defaultdict(list)
 
-        for s in strs:
-            count = [0] * 26
-            for c in s:
-                count[ord(c) - ord("a")] += 1
-            ans[tuple(count)].append(s)
-        return ans.values()
+#         for s in strs:
+#             count = [0] * 26
+#             for c in s:
+#                 count[ord(c) - ord("a")] += 1
+#             ans[tuple(count)].append(s)
+#         return ans.values()
 
                 
 
@@ -16,4 +16,20 @@ class Solution:
 
 # Input: strs = ["eat","tea","tan","ate","nat","bat"]  
            
-# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]   
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]  
+
+
+# Another solution
+
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        ans = {}
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            if tuple(count) in ans:
+                ans[tuple(count)].append(s)
+            else:
+                ans[tuple(count)] = [s]
+        return ans.values()
